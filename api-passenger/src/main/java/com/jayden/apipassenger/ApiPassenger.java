2b571d560deb920f0780d4e0d0df00cd83b2dbf7
JAYDEN\12633 <1263346845@qq.com>
2023-09-21 17:53:55 +0800

    父工程和api-passenger服务的骨架搭建

---
+++ api-passenger/src/main/java/com/jayden/apipassenger/ApiPassenger.java
@@ -0,0 +1,11 @@
+package com.jayden.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassenger {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassenger.class);
+    }
+}
