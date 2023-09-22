f372adfd11570a004fc06e277f0209fe0caa206e
JAYDEN\12633 <1263346845@qq.com>
2023-09-22 23:24:25 +0800

    根据手机号查询用户信息

---
+++ service-passenger-user/src/main/java/com/jayden/servicepassengeruser/mapper/PassengerUserMapper.java
@@ -0,0 +1,9 @@
+package com.jayden.servicepassengeruser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jayden.servicepassengeruser.dto.PassengerUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
--- service-passenger-user/src/main/java/com/jayden/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/jayden/servicepassengeruser/service/UserService.java
@@ -1,15 +1,27 @@
 package com.jayden.servicepassengeruser.service;
 
+import com.jayden.servicepassengeruser.dto.PassengerUser;
+import com.jayden.servicepassengeruser.mapper.PassengerUserMapper;
 import com.jayen.internelcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
+
     public ResponseResult loginOrReg(String passengerPhone) {
         System.out.println("userService 被调用 手机号：" + passengerPhone);
         // 根据手机号查询用户信息
-
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("passenger_phone", passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerPhone());
         // 判断用户信息是否存在
 
         //如果不沉溺在，插入用户信息
