4ddfd5bca8148ad6e28e6fa6fa3670fe407c7611
JAYDEN\12633 <1263346845@qq.com>
2023-09-23 11:22:26 +0800

    token 拦截器的编写

---
+++ api-passenger/src/main/java/com/jayden/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,46 @@
+package com.jayden.apipassenger.interceptor;
+
+
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.jayen.internelcommon.dto.ResponseResult;
+import com.jayen.internelcommon.dto.TokenResult;
+import com.jayen.internelcommon.util.JWTUtils;
+import net.sf.json.JSONObject;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        boolean result = true;
+        String resultString = "";
+        String token = request.getHeader("Authorization");
+        try{
+            TokenResult tokenResult = JWTUtils.parseToken(token);
+        }catch (SignatureVerificationException e){
+            resultString = "token sign error";
+            result = false;
+        }catch (TokenExpiredException e){
+            resultString = "token time out";
+            result = false;
+        }catch (AlgorithmMismatchException e){
+            resultString = "token AlgorithmMismatchException";
+            result = false;
+        }catch (Exception e){
+            resultString = "token invalid";
+            result = false;
+        }
+
+        if (!result){
+            PrintWriter out = response.getWriter();
+            out.print(JSONObject.fromObject(ResponseResult.fail(resultString).toString()));
+        }
+
+        return result;
+    }
+}
