063120350b381b49578fc0c10f00afc20c4c6c6f
JAYDEN\12633 <1263346845@qq.com>
2023-09-28 00:34:48 +0800

    预估价格服务骨架搭建

---
+++ api-passenger/src/main/java/com/jayden/apipassenger/controller/ForecastPriceController.java
@@ -0,0 +1,32 @@
+package com.jayden.apipassenger.controller;
+
+
+import com.jayden.apipassenger.service.ForecastPriceService;
+import com.jayen.internelcommon.dto.ResponseResult;
+import com.jayen.internelcommon.request.ForecastPriceDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ForecastPriceController {
+
+    @Autowired
+    ForecastPriceService forecastPriceService;
+
+    @PostMapping("/forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO){
+
+        String depLongitude = forecastPriceDTO.getDepLongitude();
+        String depLatitude = forecastPriceDTO.getDepLatitude();
+        String destLongitude = forecastPriceDTO.getDestLongitude();
+        String destLatitude = forecastPriceDTO.getDestLatitude();
+        String cityCode = forecastPriceDTO.getCityCode();
+        String vehicleType = forecastPriceDTO.getVehicleType();
+
+        return forecastPriceService.forecastPrice(depLongitude,depLatitude,destLongitude,destLatitude,cityCode,vehicleType);
+    }
+}
