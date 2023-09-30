0e3e04319d1177f40019078895bc92aa206c7d67
JAYDEN\12633 <1263346845@qq.com>
2023-09-30 10:04:38 +0800

    api-driver调用service-driver-user司机信息

---
+++ api-driver/src/main/java/com/jayden/apidriver/service/UserService.java
@@ -0,0 +1,35 @@
+package com.jayden.apidriver.service;
+
+
+import com.jayden.apidriver.remote.ServiceDriverUserClient;
+import com.jayden.internelcommon.dto.DriverCarBindingRelationship;
+import com.jayden.internelcommon.dto.DriverUser;
+import com.jayden.internelcommon.dto.DriverUserWorkStatus;
+import com.jayden.internelcommon.dto.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private ServiceDriverUserClient serviceDriverUserClient;
+
+    public ResponseResult updateUser(DriverUser driverUser){
+        return serviceDriverUserClient.updateUser(driverUser);
+    }
+
+    public ResponseResult changeWorkStatus(DriverUserWorkStatus driverUserWorkStatus){
+        return serviceDriverUserClient.changeWorkStatus(driverUserWorkStatus);
+    }
+
+    public ResponseResult<DriverCarBindingRelationship> getDriverCarBindingRelationship(String driverPhone){
+        // 根据driverPhone查询司机信息
+        return serviceDriverUserClient.getDriverCarRelationShip(driverPhone);
+
+    }
+
+    public ResponseResult<DriverUserWorkStatus> getWorkStatus(Long driverId){
+        return serviceDriverUserClient.getWorkStatus(driverId);
+    }
+}
